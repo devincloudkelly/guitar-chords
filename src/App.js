f5ce3df7c3f6bf835f567ab242840ebab48f1b2c
devincloudkelly <devin.cloud.kelly@gmail.com>
2022-01-08 23:01:57 -0600

    Add guitar chord library, get working example

--- src/App.js
+++ src/App.js
@@ -1,7 +1,38 @@
 import "./App.css";
+import Chord from "@tombatossals/react-chords/lib/Chord";
+
+const GuitarChord = () => {
+  const chord = {
+    frets: [1, 3, 3, 2, 1, 1],
+    fingers: [1, 3, 4, 2, 1, 1],
+    barres: [1],
+    capo: false,
+  };
+
+  const instrument = {
+    strings: 6,
+    fretsOnChord: 4,
+    name: "Guitar",
+    keys: [],
+    tunings: {
+      standard: ["E", "A", "D", "G", "B", "E"],
+    },
+  };
+
+  const lite = false;
+  return <Chord chord={chord} instrument={instrument} lite={lite} />;
+};
 
 function App() {
-  return <div className="App">Guitar Chords</div>;
+  return (
+    <div className="App">
+      Guitar Chords
+      <GuitarChord />
+      <GuitarChord />
+      <GuitarChord />
+      <GuitarChord />
+    </div>
+  );
 }
 
 export default App;
